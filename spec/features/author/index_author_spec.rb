34bf8ddfe0ccea2499a2221d9416dfd460313e46
Jacob Freise <jacob.freise@student.hpi.de>
2017-10-26 17:51:17 +0200

    Failing Test for #8.

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 describe "Authors index page", type: :feature do
+
   it "should render without error" do
     visit authors_path
+  end
+
+  it "should link to new Author page" do
+    visit authors_path
+
+    expect(page).to have_link("add author")
   end
 end

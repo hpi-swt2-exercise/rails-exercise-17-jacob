c9f95a0552d05a812b465f95dbe1e77cd57331c6
Jacob Freise <jacob.freise@student.hpi.de>
2017-10-29 11:08:04 +0100

    Failing Test for #13.

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 describe "Edit author page", type: :feature do
 
+  before do
+    @author = create(:author)
+  end
+
   it "should render without error" do
-    visit edit_author_path
+    visit edit_author_path(@author)
   end
 end
